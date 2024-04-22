cacc3815872eec07f23e57c7568cbcf9e3cb2eb9
Uday Kumar Mahto <aryanuday707@gmail.com>
2024-04-22 19:23:19 +0530

    Routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import SignIn from "./page/SignIn";
+import About from "./page/About";
+import Project from "./page/Project";
+import SignUp from "./page/SignUp";
+import Dashboard from "./page/Dashboard";
+
 function App() {
-  return <div className="text-5xl">helloworld</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
